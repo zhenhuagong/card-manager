ea7e27a0ca92c9f97eff307202f1a571b63aa78d
simongong <simon.gong64@gmail.com>
2015-11-26 14:28:49 +0800

    Added status display like progressbar in sms sending page

--- App/Views/SMS/send.android.js
+++ App/Views/SMS/send.android.js
@@ -13,7 +13,7 @@
 'use strict';
 
 let React = require('react-native');
-let { StyleSheet, Text, View, TextInput, ToastAndroid } = React;
+let { StyleSheet, Text, View, TextInput, ToastAndroid, ProgressBarAndroid } = React;
 
 let Button = require('../Shared/button');
 let Request = require('../../Networks/request');
@@ -21,27 +21,19 @@ let Configs = require('../../configs');
 let { Icon } = require('react-native-icons');
 let FlashData = require('../../Networks/flashData');
 
-let FormBlock = React.createClass({
-  render() {
-    let formElementStyle = {
-      flexDirection: 'column',   // column direction by default
-      alignItems: 'center',
-      marginTop: 20
-    };
-    return (
-      <View style={[formElementStyle, this.props.style]}>
-        {this.props.children}
-      </View>
-    );
-  }
-});
-
 let SendSMS = React.createClass({
   getInitialState() {
     return {
       number: '',
-      text: ''
+      text: '',
+      delivered: 'no',
+      error: '',
+      editable: true
     };
+  },
+
+  componentWillUpdate(nextProps, nextState) {
+    nextState.editable = nextState.delivered === 'pending' ? false : true;
   },
 
   render() {
@@ -68,32 +60,64 @@ let SendSMS = React.createClass({
     }
     contents.push(_text);
 
+    let deliverNode, errorNode;
+    switch(this.state.delivered) {
+      case 'pending':
+        deliverNode = (
+          <View style={{flexDirection: 'row'}}>
+            <ProgressBarAndroid styleAttr='Small'/>
+            <Text style={{marginLeft: 20}}>正在发送...</Text>
+          </View>
+        );
+        break;
+      case 'yes':
+        deliverNode = (
+          <View style={{flexDirection: 'row'}}>
+            <Icon
+              name='material|check'
+              size={24}
+              style={styles.sendIcon} />
+            <Text style={{marginLeft: 20}}>已发送</Text>
+          </View>
+        );
+        break;
+      default:
+        break;
+    }
+
+    if (this.state.error) {
+      errorNode = ( <Text style={styles.error}> {this.state.error} </Text> );
+    }
+
     return (
       <View style={styles.container}>
-        <FormBlock>
+        <View style={styles.formItem}>
           <TextInput style={styles.input}
             keyboardType='numeric'
-            placeholder='请输入卡号'
+            placeholder='卡号'
+            editable={this.state.editable}
             value={this.state.number}
             onChangeText={(number) => {
               this.setState({number});
             }}/>
-        </FormBlock>
-        <FormBlock>
+        </View>
+        <View style={styles.formItem}>
           <TextInput style={[styles.input, styles.inputArea]} multiline={true}
             autoCapitalize='none' autoCorrect={false}
-            placeholder='请输入短信内容' textAlign='start' textAlignVertical='top'
+            editable={this.state.editable}
+            placeholder='短信内容' textAlign='start' textAlignVertical='top'
             onChangeText={(text) => {
               this.setState({text});
             }}>
             <Text>{contents}</Text>
           </TextInput>
           <Text style={styles.registerText}>（通配符: 姓名:%name% , 尊称:%title%）</Text>
-        </FormBlock>
-        <FormBlock>
-          <Text style={styles.error}>{this.state.error}</Text>
-        </FormBlock>
-        <FormBlock style={{marginTop: 30}}>
+        </View>
+        <View style={styles.formItem}>
+          {deliverNode}
+          {errorNode}
+        </View>
+        <View style={{marginTop: 30}}>
           <Button onPress={this._send}>
             <Text style={{color: '#ffffff'}}>发送</Text>
             <Icon
@@ -102,14 +126,14 @@ let SendSMS = React.createClass({
               color='#ffffff'
               style={styles.sendIcon} />
           </Button>
-        </FormBlock>
+        </View>
       </View>
       )
   },
 
   _send() {
     if (this._validateInput()) {
-      this.setState({ error: '' });
+      this.setState({ error: '', delivered: 'pending'});
       Request.get(
         Configs.endpoints.sendSMS,
         {
@@ -122,14 +146,20 @@ let SendSMS = React.createClass({
         console.log('message sent' + JSON.stringify(response));
         switch(response.result) {
           case '00':
+            this.setState({error: '', delivered: 'yes'});
             ToastAndroid.show('发送成功！', ToastAndroid.SHORT);
-            this.props.navigator.pop();
+            setTimeout(() => {
+              this.props.navigator.pop();
+            }, 1000);
             break;
           case '01':
-            this.setState({ error: '发送短信失败！请稍后再试。' });
+            this.setState({ error: '发送短信失败！请稍后再试。', delivered: 'no' });
             break;
           default:
-            this.setState({ error: '服务器异常，暂时无法发送短信，请稍后再试' });
+            this.setState({
+              error: '服务器异常，暂时无法发送短信，请稍后再试',
+              delivered: 'no'
+            });
             break;
         }
       })
@@ -137,7 +167,7 @@ let SendSMS = React.createClass({
         console.log('got error when sending message ' + error);
       });
     } else {
-      this.setState({ error: '提示：卡号和短信内容不能为空！' });
+      this.setState({ error: '提示：卡号和短信内容不能为空！', delivered: 'no' });
     }
   },
 
@@ -158,6 +188,10 @@ let styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     height: 180
   },
+  formItem: {
+    alignItems: 'center',
+    marginTop: 20
+  },
   input: {
     width: 180,
     height: 40,
@@ -165,8 +199,7 @@ let styles = StyleSheet.create({
     padding: 4,
   },
   inputArea: {
-    height: 60,
-    marginBottom: 10,
+    height: 60
   },
   highlight: {
     color: 'blue',
